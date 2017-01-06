037b5e8e0256d1f055c961b170367c7c072629de
Alexey Merezhin <amerezhin@e3ag.ch>
2017-01-06 14:15:15 +0100

    upd plugin with latest recommendations from jenkins

--- src/main/java/org/jenkinsci/plugins/pipeline/dependency/flow/FlowTriggerStep.java
+++ src/main/java/org/jenkinsci/plugins/pipeline/dependency/flow/FlowTriggerStep.java
@@ -1,48 +1,96 @@
 package org.jenkinsci.plugins.pipeline.dependency.flow;
 
-import org.jenkinsci.plugins.workflow.steps.AbstractStepDescriptorImpl;
-import org.jenkinsci.plugins.workflow.steps.AbstractStepImpl;
-import org.jenkinsci.plugins.workflow.util.StaplerReferer;
-import org.kohsuke.accmod.Restricted;
-import org.kohsuke.accmod.restrictions.DoNotUse;
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+import org.jenkinsci.Symbol;
 import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
+import hudson.console.ModelHyperlinkNote;
 import hudson.maven.MavenModuleSet;
+import hudson.model.AbstractProject;
 import hudson.model.AutoCompletionCandidates;
+import hudson.model.DependencyGraph;
 import hudson.model.ItemGroup;
-import hudson.model.Job;
-import jenkins.model.ParameterizedJobMixIn;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import jenkins.model.Jenkins;
+import jenkins.tasks.SimpleBuildStep;
 
 /**
  * Created by e3cmea on 1/6/17.
  *
  * @author Alexey Merezhin
  */
-public class FlowTriggerStep extends AbstractStepImpl {
-    private String job;
+public class FlowTriggerStep extends Builder implements SimpleBuildStep {
+    private static final Logger LOGGER = Logger.getLogger(FlowTriggerStep.class.getName());
+    private @CheckForNull String job;
 
     @DataBoundConstructor
     public FlowTriggerStep(String job) {
         this.job = job;
     }
 
-    public String getJob() {
+    public @Nonnull String getJob() {
         return job;
     }
 
-    @Extension
-    public static class DescriptorImpl extends AbstractStepDescriptorImpl {
+    @Override
+    public void perform(@Nonnull Run<?, ?> run, @Nonnull FilePath filePath, @Nonnull Launcher launcher,
+            @Nonnull TaskListener taskListener) throws InterruptedException, IOException {
+        LOGGER.info("Start flow with root job: " + getJob());
 
-        public DescriptorImpl() {
-            super(FlowTriggerStepExecution.class);
+        Jenkins jenkins = Jenkins.getActiveInstance();
+        MavenModuleSet rootProject = jenkins.getItemByFullName(getJob(), MavenModuleSet.class);
+
+        Set<AbstractProject> projects = new LinkedHashSet<>();
+        createProjectList(rootProject, projects);
+
+        for (AbstractProject project : projects) {
+            taskListener.getLogger().println("Trigger job: " + ModelHyperlinkNote.encodeTo("/" + project.getUrl(), project.getFullDisplayName()));
+
+            LOGGER.info("do it for project " + project.getName());
         }
+    }
 
+    /**
+     * Create a list of dependent projects, first element is in the bottom of dependency graph
+     * @param job top project's name
+     * @param projects resulting list
+     */
+    private void createProjectList(AbstractProject project, Set<AbstractProject> projects) {
+        for (AbstractProject currentProject: getUpstream(project)) {
+            createProjectList(currentProject, projects);
+        }
+        projects.add(project);
+    }
+
+    private List<AbstractProject> getUpstream(AbstractProject project) {
+        Jenkins jenkins = Jenkins.getActiveInstance();
+        // MavenModuleSet rootProject = jenkins.getItemByFullName(jobName, MavenModuleSet.class);
+        DependencyGraph dependencyGraph = jenkins.getDependencyGraph();
+        return dependencyGraph.getUpstream(project);
+    }
+
+    @Symbol("flowexec")
+    @Extension
+    public static class DescriptorImpl extends BuildStepDescriptor<Builder> {
         @Override
-        public String getFunctionName() {
-            return "flowexec";
+        public boolean isApplicable(Class<? extends AbstractProject> t) {
+            return true;
         }
 
         @Override
@@ -54,13 +102,6 @@ public class FlowTriggerStep extends AbstractStepImpl {
                 @QueryParameter String value) {
             return AutoCompletionCandidates.ofJobNames(MavenModuleSet.class, value, context);
         }
-
-        @Restricted(DoNotUse.class) // for use from config.jelly
-        public String getContext() {
-            Job<?,?> job = StaplerReferer.findItemFromRequest(Job.class);
-            return job != null ? job.getFullName() : null;
-        }
-
     }
 
 }
